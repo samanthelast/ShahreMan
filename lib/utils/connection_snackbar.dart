091ce3fa69c15f9fc858c000c881e6ba586eab3b
Saman Ariyanpour <samanthelast@gmail.com>
2019-10-16 19:59:00 +0330

    adding files

---
+++ lib/utils/connection_snackbar.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/material.dart';
+
+
+// class SnackBarPage extends StatefulWidget{
+//   BuildContext context;
+//   SnackBarPage(this.context);
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     return _SnackBarPage(context);
+//   }
+// }
+
+// class _SnackBarPage extends State<SnackBarPage> {
+//   BuildContext context;
+//   _SnackBarPage(this.context);
+//   @override
+//   Widget build(BuildContext context) {
+//     final snackBar = SnackBar(
+//             content: Text('Yay! A SnackBar!'),
+//             action: SnackBarAction(
+//               label: 'Undo',
+//               onPressed: () {
+//                 // Some code to undo the change.
+//               },
+//             ),
+//           );
+          
+
+
+  
+//   Scaffold.of(context).showSnackBar(snackBar);
+
+//     return Text('hi');
+//   }
+// }
